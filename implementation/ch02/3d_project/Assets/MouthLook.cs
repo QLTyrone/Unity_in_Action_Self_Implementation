6cce51631c751f5bf2bb62cb6a148f474af816e7
QLTyrone <hly020523@gmail.com>
2025-03-11 20:57:09 +0800

    feat: Mouth Look

--- implementation/ch02/3d_project/Assets/MouthLook.cs
+++ implementation/ch02/3d_project/Assets/MouthLook.cs
@@ -9,6 +9,12 @@ public class MouthLook : MonoBehaviour
     }
     public RotationAxes axes = RotationAxes.MouthXAndY;
     public float sensitivityHor = 9.0f;
+    public float sensitivityVert = 9.0f;
+
+    public float minimumVert = -45.0f;
+    public float maximumVert = 45.0f;
+
+    private float _rotationX = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,13 +25,20 @@ public class MouthLook : MonoBehaviour
     void Update()
     {
         if (axes == RotationAxes.MouthX) {
-            transform.Rotate(0, Input.GetAxis("Mouth X")*sensitivityHor, 0);
+            transform.Rotate(0, Input.GetAxis("Mouse X")*sensitivityHor, 0);
         }
         else if (axes == RotationAxes.MouthY) {
-            // vertical rotation here
+            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+            _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+            float rotationY = transform.localEulerAngles.y;
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
         }
         else {
-            // both horizontal and vertical rotation here
+            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+            _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+            float delta = Input.GetAxis("Mouse X") * sensitivityHor;
+            float rotationY = transform.localEulerAngles.y + delta;
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
         }
     }
 }
