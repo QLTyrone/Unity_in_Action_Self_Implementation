19af462caad4cd2a3e43b177501b6beb1308d27f
QLTyrone <hly020523@gmail.com>
2025-03-17 19:02:29 +0800

    feat: show hit

--- implementation/ch02/3d_project/Assets/Scripts/RayShooter.cs
+++ implementation/ch02/3d_project/Assets/Scripts/RayShooter.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class RayShooter : MonoBehaviour
 {
@@ -7,6 +8,8 @@ public class RayShooter : MonoBehaviour
     void Start()
     {
         _camera = GetComponent<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
@@ -17,8 +20,26 @@ public class RayShooter : MonoBehaviour
             Ray ray = _camera.ScreenPointToRay(point);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit)) {
-                Debug.Log("Hit "+hit.point);
+                // Debug.Log("Hit "+hit.point);
+                StartCoroutine(SphereIndicator(hit.point));
             }
         }
+    }
+
+    // 这个函数会返回一个IEnumerator对象
+    private IEnumerator SphereIndicator(Vector3 pos) {
+        // sphere是球体
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = pos;
+        // yield return是暂停点，而不是传统意义的return
+        yield return new WaitForSeconds(1);
+        Destroy(sphere);
+    }
+
+    void OnGUI() {
+        int size = 12;
+        float posX = _camera.pixelWidth/2 - size/4;
+        float posY = _camera.pixelHeight/2 - size/2;
+        GUI.Label(new Rect(posX, posY, size, size), "*");
     }
 }
