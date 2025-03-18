ff1241eb3bd9ff2fe53012570e09709ab589fe21
QLTyrone <hly020523@gmail.com>
2025-03-18 18:41:39 +0800

    feat: wandering AI

---
+++ implementation/ch02/3d_project/Assets/Scripts/WanderingAI.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections;
+
+public class WanderingAI : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public float obstacleRange = 5.0f;
+    private bool _alive;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _alive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_alive){
+            transform.Translate(0, 0, speed * Time.deltaTime);
+
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+            // 三个参数，第一个是方向，第二个是半径（在射线周围多远），第三个是结果
+            if (Physics.SphereCast(ray, 0.75f, out hit)) {
+                if (hit.distance<obstacleRange) {
+                    float angle = Random.Range(-110, 110);
+                    transform.Rotate(0, angle, 0);
+                }
+            }
+        }
+    }
+
+    public void SetAlive(bool alive) {
+        _alive = alive;
+    }
+}
