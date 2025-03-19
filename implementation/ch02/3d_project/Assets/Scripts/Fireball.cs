7f99abdab8500e4de240c5f7d06edac71d9b2582
QLTyrone <hly020523@gmail.com>
2025-03-19 19:49:00 +0800

    feat: fireball usage

--- implementation/ch02/3d_project/Assets/Scripts/Fireball.cs
+++ implementation/ch02/3d_project/Assets/Scripts/Fireball.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Fireball : MonoBehaviour
 {
+    public float speed = 10.0f;
+    public int damage = 1;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,13 @@ public class Fireball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+    void OnTriggerEnter(Collider other) {
+        PlayerCharacter player = other.GetComponent<PlayerCharacter>();
+        if (player != null) {
+            Debug.Log("Player hit");
+        }
+        Destroy(this.gameObject);
     }
 }
