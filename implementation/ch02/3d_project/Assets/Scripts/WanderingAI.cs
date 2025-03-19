86c64fdd8a618ae5c25af7d0569e741ce69c844f
QLTyrone <hly020523@gmail.com>
2025-03-19 19:40:00 +0800

    feat: create fireball

--- implementation/ch02/3d_project/Assets/Scripts/WanderingAI.cs
+++ implementation/ch02/3d_project/Assets/Scripts/WanderingAI.cs
@@ -6,6 +6,10 @@ public class WanderingAI : MonoBehaviour
     public float speed = 3.0f;
     public float obstacleRange = 5.0f;
     private bool _alive;
+
+    [SerializeField] private GameObject fireballPrefab;
+    private GameObject _fireball;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -22,7 +26,15 @@ public class WanderingAI : MonoBehaviour
             RaycastHit hit;
             // 三个参数，第一个是方向，第二个是半径（在射线周围多远），第三个是结果
             if (Physics.SphereCast(ray, 0.75f, out hit)) {
-                if (hit.distance<obstacleRange) {
+                GameObject hitObject = hit.transform.gameObject;
+                if (hitObject.GetComponent<PlayerCharacter>()) {
+                    if (_fireball == null) {
+                        _fireball = Instantiate(fireballPrefab) as GameObject;
+                        _fireball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
+                        _fireball.transform.rotation = transform.rotation;
+                    }
+                }
+                else if (hit.distance<obstacleRange) {
                     float angle = Random.Range(-110, 110);
                     transform.Rotate(0, angle, 0);
                 }
